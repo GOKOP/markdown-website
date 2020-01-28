a0e5c4a9f5ac31168ead1b655933e6bd8f70b1c0
GOKOP <igorbugajski@protonmail.com>
2020-01-28 17:47:15 +0100

    walking through git files

--- sitedata/sitedata.go
+++ sitedata/sitedata.go
@@ -1,5 +1,11 @@
 package sitedata
 
+import (
+	"log"
+	"os"
+	"path/filepath"
+)
+
 type MenuEntry struct {
 	Title string
 	Dest  string
@@ -9,4 +15,33 @@ type Page struct {
 	Title   string
 	Content string
 	Menu []MenuEntry
+}
+
+func Get() Page {
+
+	page := Page {
+		Title:   "404 Not found",
+		Content: "<h1>404: Page not found</h1",
+		Menu:    nil,
+	}
+
+	err := filepath.Walk("website", func(path string, info os.FileInfo, err error) error {
+
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		// to do
+		return nil
+	})
+
+	if err != nil {
+		log.Print("filepath.Walk: "+err.Error())
+	}
+
+	return page
 }
